0b73569e7a869ea00a0bd640d8531c91bed44dcd
jakimfett <jakimfett@gmail.com>
2015-07-30 11:16:27 -0700

    Rework key listener a bit

--- client/js/connect.js
+++ client/js/connect.js
@@ -5,84 +5,132 @@ var queue = [];
 var x, y;
 
 function init() {
-    canvas = document.getElementById('canvas');
-    if ("WebSocket" in window) {
-        ws = new WebSocket("ws://localhost:1357");
-        addEventListeners();
-    } else
-    {
-        // The browser doesn't support WebSocket
-        alert("WebSocket NOT supported by your Browser!");
-    }
+	canvas = document.getElementById('canvas');
+	if ("WebSocket" in window) {
+		ws = new WebSocket("ws://localhost:1357");
+		addEventListeners();
+	} else
+	{
+		// The browser doesn't support WebSocket
+		alert("WebSocket NOT supported by your Browser!");
+	}
 }
 
 function addEventListeners() {
-    canvas.addEventListener('mousemove', function (evt) {
-        mousePosition = getMousePos(canvas, evt);
-    }, false);
+	initMouseListeners();
 
-    canvas.addEventListener('onclick', function (evt) {
-        console.log("Mouse is at x:" + mousePosition.x + " y:" + mousePosition.y);
-    }, false);
+	initKeyboardListeners();
 
-    window.addEventListener("keypress", doKeyDown, false);
+	initWebSocketListeners();
+}
 
-    // Websocket listener
-    ws.onopen = function () {
-        console.log('Socket open');
-    };
+function initMouseListeners(){
+	canvas.addEventListener('mousemove', function (evt) {
+		mousePosition = getMousePos(canvas, evt);
+	}, false);
 
-    ws.onclose = function () {
-        console.log('Socket closed');
-    };
+	canvas.addEventListener('onclick', function (evt) {
+		console.log("Mouse is at x:" + mousePosition.x + " y:" + mousePosition.y);
+	}, false);
+}
 
-    ws.onerror = function (err) {
-        console.log("Error: " + err);
-    }
+function initWebSocketListeners() {
+	ws.onopen = function () {
+		console.log('Socket open');
+	};
 
-    ws.onmessage = function (message)
-    {
-        console.log('received: %s', message.data);
-    };
+	ws.onclose = function () {
+		console.log('Socket closed');
+	};
+
+	ws.onerror = function (err) {
+		console.log("Error: " + err);
+	}
+
+	ws.onmessage = function (message)
+	{
+		console.log('received: %s', message.data);
+	};
+}
+function initKeyboardListeners() {
+
+	if (document.addEventListener)
+	{
+		document.addEventListener("keydown", keyEvent, false);
+		document.addEventListener("keypress", keyEvent, false);
+		document.addEventListener("keyup", keyEvent, false);
+	}
+	else if (document.attachEvent)
+	{
+		document.attachEvent("onkeydown", keyEvent);
+		document.attachEvent("onkeypress", keyEvent);
+		document.attachEvent("onkeyup", keyEvent);
+	}
+	else
+	{
+		document.onkeydown = keyEvent;
+		document.onkeypress = keyEvent;
+		document.onkeyup = keyEvent;
+	}
+}
+
+
+function keyEvent(e){
+	if(!e){
+		e = event;
+	}
+	console.log(e.type+": ", getKeyValueFromCode(e.which));
+	return false;
+}
+
+
+function getKeyValueFromCode(code){
+	if(code === 'undefined'){
+		return 'undefined';
+	}
+	if (code >= 32 && code < 127){
+		return String.fromCharCode(code);
+	}
+	return code;
 }
 
 function sendMessage(msg) {
 
-    if (ws.readyState !== 1) {
-        attemptReconnect();
-    }
-    addToQueue(msg);
-    if (ws.readyState === 1) {
-        processQueue();
-    }
+	if (ws.readyState !== 1) {
+		attemptReconnect();
+	}
+	addToQueue(msg);
+	if (ws.readyState === 1) {
+		processQueue();
+	}
 }
 
 function attemptReconnect() {
-    if (ws.readyState !== 1) {
-        ws = new WebSocket("ws://localhost:1357");
-    }
+	if (ws.readyState !== 1) {
+		ws = new WebSocket("ws://localhost:1357");
+	}
 }
 function processQueue() {
-    if (queue.length > 1) {
-        //verifyPosition();
-    }
-    if (queue.length > 0) {
-        for (var i = 0; i < queue.length; i++) {
-            var msg = queue.pop();
-            console.log("Processing message: " + msg);
-            ws.send(msg);
-        }
-    }
+	if (queue.length > 1) {
+		//verifyPosition();
+	}
+	if (queue.length > 0) {
+		for (var i = 0; i < queue.length; i++) {
+			var msg = queue.pop();
+			console.log("Processing message: " + msg);
+			ws.send(msg);
+		}
+	}
 }
 
 function addToQueue(msg) {
-    console.log("Queuing message: " + msg);
-    queue.push(msg);
+	console.log("Queuing message: " + msg);
+	queue.push(msg);
 
 
 }
 function doKeyDown(e) {
-    sendMessage(e.keyCode);
+	//sendMessage(e.key);
 }
 
 
@@ -95,10 +143,10 @@ function doKeyDown(e) {
 
 
 function getMousePos(canvas, evt) {
-    var rect = canvas.getBoundingClientRect();
-    return {
-        x: evt.clientX - rect.left,
-        y: evt.clientY - rect.top
-    };
+	var rect = canvas.getBoundingClientRect();
+	return {
+		x: evt.clientX - rect.left,
+		y: evt.clientY - rect.top
+	};
 }
 
