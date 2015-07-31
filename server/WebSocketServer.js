fda467baa9fd943bfc8c11980bff384720f00249
jakimfett <jakimfett@picraft.com>
2015-07-30 20:19:34 -0700

    Add message saving per client

--- server/WebSocketServer.js
+++ server/WebSocketServer.js
@@ -2,10 +2,23 @@
 var WebSocket = require('ws');
 var WebSocketServer = WebSocket.Server;
 var wss = new WebSocketServer({port: 1357});
+var CLIENTS = JSON.parse('{}');
 
 wss.on('connection', function (ws) {
     ws.on('message', function (message) {
-        console.log('received: %s', message);
+        //console.log('received: %s', message);
+        var msg = JSON.parse(message);
+        if ("uuid" in msg) {
+            if (!(msg.uuid in CLIENTS)) {
+                CLIENTS[msg.uuid] = {x: 0, y: 0, input: {click: [], key: []}};
+            }
+            if ("click" in msg) {
+                CLIENTS[msg.uuid]['input']['click'].push(msg.click);
+            }
+            if ("key" in msg) {
+                CLIENTS[msg.uuid]['input']['key'].push(msg.key);
+            }
+        }
         ws.send(message);
     });
 
